fcf9ad333b64e1bfdb0d76249b0b1cef94821a81
sachiveanand.a2022@vitstudent.ac.in <sachiveanand.a2022@vitstudent.ac.in>
2024-04-19 22:23:28 +0530

    Add files to the new folder

--- script.js
+++ script.js
@@ -68,3 +68,16 @@ window.addEventListener('resize', adjustLayout);
 
 // Call adjustLayout function on page load
 adjustLayout();
+
+// Smooth scrolling when links are clicked
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      const target = document.querySelector(this.getAttribute('href'));
+      target.scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
